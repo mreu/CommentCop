709bbeef29058e6ced5a13440e059f5ca6aadfa6
Michael Reukauff <michael@reukauff.de>
2016-04-17 14:03:25 +0200

    Set version to 0.8.3

--- XmlCommenter/Properties/AssemblyInfo.cs
+++ XmlCommenter/Properties/AssemblyInfo.cs
@@ -11,11 +11,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("XmlDocAnalyzer")]
-[assembly: AssemblyDescription("Analyzer and Codefix for Xml comments in Visual Studio 2015")]
+[assembly: AssemblyDescription("Analyzer and Codefix for documentations in Visual Studio 2015")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("XmlDocAnalyzer")]
-[assembly: AssemblyCopyright("Copyright ©  2016 Michael Reukauff, Germany")]
+[assembly: AssemblyCopyright("Copyright © 2016 Michael Reukauff, Germany")]
 [assembly: AssemblyTrademark("All rights reserved.")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8.*")]
+[assembly: AssemblyFileVersion("0.8.3.0")]
